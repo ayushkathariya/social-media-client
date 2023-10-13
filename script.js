55daefeca85f7a20b497d17794b2196dbf4e5f8a
Ayush <ayushkathariyua@ggmail.com>
2023-10-13 13:53:25 +0545

    script removed

--- script.js
+++
@@ -1,8 +0,0 @@
-const htmlElement = document.querySelector("html");
-const mode = localStorage.getItem("theme");
-
-if (mode === "light") {
-  htmlElement.setAttribute("data-theme", "light");
-} else {
-  htmlElement.setAttribute("data-theme", "dark");
-}
