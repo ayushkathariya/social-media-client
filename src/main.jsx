e72dd6a6ccca34fd8db3587cdb5365868d5333fc
Ayush Kathariya <ayushkathariya108@gmail.com>
2023-10-19 12:03:24 +0545

    message added

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
+// Crated by Ayush Kathariya
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
@@ -15,3 +17,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </Provider>
   </React.StrictMode>
 );
+
